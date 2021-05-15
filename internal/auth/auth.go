afee20f6debcf7752557dee154b835ff540b7699
Tom Bowyer <tom4310@gmail.com>
2021-05-16 08:00:35 +1000

    Fix lint

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -57,11 +57,8 @@ func generateRandomBytes(n int) ([]byte, error) {
 	b := make([]byte, n)
 
 	_, err := rand.Read(b)
-	if err != nil {
-		return nil, err
-	}
 
-	return b, nil
+	return b, err
 }
 
 func generateRandomString(s int) (string, error) {
