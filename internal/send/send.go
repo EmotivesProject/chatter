e13c1263933e10744b799ec4af35177904a82e01
Tom Bowyer <tom4310@gmail.com>
2021-07-27 20:23:01 +1000

    update message contents

--- internal/send/send.go
+++ internal/send/send.go
@@ -8,17 +8,15 @@ import (
 	"github.com/TomBowyerResearchProject/common/notification"
 )
 
-func MessageNotification(fromUser, newUsername string) {
+func MessageNotification(fromUser, newUsername, content string) {
 	notif := notification.Notification{
 		Username:   newUsername,
 		Type:       notification.Message,
 		Title:      "New message!",
-		Message:    fmt.Sprintf("%s messaged you", fromUser),
+		Message:    fmt.Sprintf("%s messaged you: %s", fromUser, content),
 		Link:       fmt.Sprintf("%smessenger?talking-to=%s", os.Getenv("EMOTIVES_URL"), fromUser),
 		UsernameTo: &newUsername,
 	}
-
-	logger.Infof("%s", os.Getenv("NOTIFICATION_AUTH"))
 
 	_, err := notification.SendEvent(os.Getenv("NOTIFICATION_URL"), os.Getenv("NOTIFICATION_AUTH"), notif)
 	if err != nil {
